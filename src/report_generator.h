992912355693130db3eb5fc5ba34e38669e8c4d2
Greekforce1821 <spiroskoeu@gmail.com>
2023-11-24 01:11:32 +0200

    uploaded the makefile and other useful files

---
+++ src/report_generator.h
@@ -0,0 +1,22 @@
+/* Λειτουργικά Συστήματα | Επεισόδιο ΙΙ | Ομάδα Χρηστών ΙΙΙ | Παραλλαγή της cp γνωστή και ως mycp */
+
+/* -------------------------------------------------------------- */
+
+/* Τμήμα εισαγωγής βιβλιοθηκών εισόδου / εξόδου στην C */
+
+#ifndef REPORT_GENERATOR_H
+#define REPORT_GENERATOR_H
+
+#define SUCCESS 0
+
+/*  Δήλωση της συνάρτησης generateReport() με παραμ΄΄έτρους:
+    double cpu_time_used, int buffer_size, long file_size */
+
+int generateReport(double cpu_time_used, int buffer_size, long file_size);
+
+#endif /* REPORT_GENERATOR_H */
+
+/* -------------------------------------------------------------- */
+
+/* You reached the end of the following file :D 
+   CHRISTOS - SPYRIDON KARYDIS & SPYRIDON - EFTYCHIOS KOKOTOS */
